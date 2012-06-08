823c478553a5bc05517521bd20d0af33cc4579c9
George Hickman <george@ghickman.co.uk>
2012-06-09 00:24:30 +0100

    Tidy up setup

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ install_requires = ('django-admin-sso',)
 setup(
     name='incuna-auth-urls',
     version='0.1',
-    url = 'http://github.com/incuna/incuna-auth-urls',
+    url='http://github.com/incuna/incuna-auth-urls',
     py_modules=('auth_urls',),
     include_package_data=True,
     install_requires=install_requires,
-    description = 'Provides educational modules.',
-    author = 'Incuna Ltd',
-    author_email = 'admin@incuna.com',
+    description='Provides educational modules.',
+    author='Incuna Ltd',
+    author_email='admin@incuna.com',
 )
 
