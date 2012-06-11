7faf69f9099f47e623287467fbe6cabda4a2ade7
George Hickman <george@ghickman.co.uk>
2012-06-11 16:53:21 +0100

    Add contrib.auth login/logout urls

--- auth/urls.py
+++ auth/urls.py
@@ -4,9 +4,8 @@ from django.views.generic import RedirectView
 
 
 urlpatterns = patterns('',
-    # TODO: Find the right urls for these
-    url(r'login/$', '', name='auth_login'),
-    url(r'^logout/$', '', {'template_name': 'registration/logout.html'}, name='auth_logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='auth_login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}, name='auth_logout'),
     url(r'^sso/$', RedirectView.as_view(url=reverse_lazy('admin:admin_sso_openiduser_start')), name='sso_login'),
 )
 
