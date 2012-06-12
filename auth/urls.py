545fda6b798a91839775ba57d0b7034697ae088a
George Hickman <george@ghickman.co.uk>
2012-06-12 10:09:19 +0100

    Update url reverses

--- auth/urls.py
+++ auth/urls.py
@@ -3,9 +3,12 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
 
-urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='auth_login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}, name='auth_logout'),
+urlpatterns = patterns('django.contrib.auth.views',
+    url(r'^login/$', 'login', name='auth_login'),
+    url(r'^logout/$', 'logout', {'template_name': 'registration/logout.html'}, name='auth_logout'),
+    url(r'^password/confirm', 'password_reset_confirm', name='auth_password_reset_confirm'),
+    url(r'^password/done', 'password_reset_done', name='auth_password_reset_done'),
+    url(r'^password/reset', 'password_reset', name='auth_password_reset'),
     url(r'^sso/$', RedirectView.as_view(url=reverse_lazy('admin:admin_sso_openiduser_start')), name='sso_login'),
 )
 
