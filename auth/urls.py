c65b9f06a29865a61d9185b1e33b7c5f224d03ef
George Hickman <george@ghickman.co.uk>
2012-06-17 15:27:43 +0100

    Update the auth form

--- auth/urls.py
+++ auth/urls.py
@@ -1,10 +1,17 @@
+from django import forms
 from django.conf.urls.defaults import patterns, url
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import RedirectView
 
 
+class IncunaAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label=_('Email'), max_length=320)
+
+
 urlpatterns = patterns('django.contrib.auth.views',
-    url(r'^login/$', 'login', name='auth_login'),
+    url(r'^login/$', 'login', {'authentication_form': AuthenticationForm}, name='auth_login'),
     url(r'^logout/$', 'logout', {'template_name': 'registration/logout.html'}, name='auth_logout'),
     url(r'^password/confirm', 'password_reset_confirm', name='auth_password_reset_confirm'),
     url(r'^password/done', 'password_reset_done', name='auth_password_reset_done'),
