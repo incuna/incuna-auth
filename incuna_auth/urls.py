0a412f9585ba293eddfc0eee2dacf061f91e6046
mattack108 <matt.lenc@gmail.com>
2013-04-05 16:40:55 +0100

    Fix the borken urls

--- incuna_auth/urls.py
+++ incuna_auth/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('django.contrib.auth.views',
     url(_(r'^password/change/done/$'), 'password_change_done', name='password_change_done'),
     # Reset password (when not logged in via unique email link)
     url(_(r'^password/reset/$'), 'password_reset', {'password_reset_form': CrispyPasswordResetForm}, name='auth_password_reset'),
-    url(_(r'^password/reset/done/$', 'password_reset_done'), name='auth_password_reset_done'),
+    url(_(r'^password/reset/done/$'), 'password_reset_done', name='auth_password_reset_done'),
     url(_(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$'), 'password_reset_confirm', name='auth_password_reset_confirm'),
     url(_(r'^password/reset/complete/$'), 'password_reset_complete', name='auth_password_reset_complete'),
     url(_(r'^sso/$'), RedirectView.as_view(url=reverse_lazy('admin:admin_sso_openiduser_start')), name='sso_login'),
