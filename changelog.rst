e59b27d82c51f77282d24bac07ee7c2ac8cb5ba2
name to commit with <email>
2014-03-18 15:07:32 +0000

    Backwards compatibility for uidb36 urls

--- changelog.rst
+++ changelog.rst
@@ -1,24 +1,29 @@
 Changelog
 =========
 
+v2.0.1
+-------
+* Add backwards compatibility to differentiate the urls used in Django <1.6
+  and Django >= 1.6.
+
 v2.0.0
 -------
 *Backwards incompatible: urls renamed to match django >= 1.6.*
 
 * Django > 1.4 added names to `contrib.auth.urls`. Django >= 1.6 started using
-  the url names in views. 
+  the url names in views.
   If you are using Django >= 1.6 then you will need to update your views and
   templates to reverse the auth urls using the new names. Remove the `auth_`
   prefix from the following urls:
-  
-  * `auth_login` > `login`  
-  * `auth_logout` > `logout`  
+
+  * `auth_login` > `login`
+  * `auth_logout` > `logout`
   * `auth_password_change` > `password_change`
   * `auth_password_reset` > `password_reset`
   * `auth_password_reset_done` > `password_reset_done`
   * `auth_password_reset_confirm` > `password_reset_confirm`
   * `auth_password_reset_complete` > `password_reset_complete`
-  
+
   If you are using Django < 1.6 then you can continue using the old auth url
   names then create and include a project specific `auth_urls` using the old
   names.
@@ -27,9 +32,9 @@ v2.0.0
   be changed to `{% url 'password_reset_confirm' uidb64=uid token=token %}`
 * Remove `forms.IncunaAuthenticationForm`. Django >= 1.6 provides an
   `AuthenticationForm` with a `username` field with `max_length=254` and a label
-  based on the user model's `USERNAME_FIELD`. If you are extending 
+  based on the user model's `USERNAME_FIELD`. If you are extending
   `incuna_auth.forms.IncunaAuthenticationForm` in your project then you should
-  now extend `django.contrib.auth.forms.AuthenticationForm` and consider adding 
+  now extend `django.contrib.auth.forms.AuthenticationForm` and consider adding
   `username = forms.CharField(label=_('Email'), max_length=320, widget=forms.TextInput(attrs={'type': 'email'}))`
 
 v1.0.0
--- incuna_auth/urls.py
+++ incuna_auth/urls.py
@@ -1,3 +1,4 @@
+import django
 from django.conf import settings
 from django.conf.urls import patterns, url
 from django.core.urlresolvers import get_callable, reverse_lazy
@@ -7,6 +8,8 @@ from django.views.generic import RedirectView
 auth_form = get_callable(getattr(settings, 'INCUNA_AUTH_LOGIN_FORM', 'django.contrib.auth.forms.AuthenticationForm'))
 reset_form = get_callable(getattr(settings, 'INCUNA_PASSWORD_RESET_FORM', 'incuna_auth.forms.CrispyPasswordResetForm'))
 
+# Check the Django version is <= 1.6 (django.version returns a tuple)
+# The _uidb36 will be removed in Django 1.7 so we want to check for that too.
 
 urlpatterns = patterns('django.contrib.auth.views',
     url(_(r'^login/$'), 'login', {'authentication_form': auth_form}, name='login'),
@@ -17,13 +20,29 @@ urlpatterns = patterns('django.contrib.auth.views',
     # Reset password (when not logged in via unique email link)
     url(_(r'^password/reset/$'), 'password_reset', {'password_reset_form': reset_form}, name='password_reset'),
     url(_(r'^password/reset/done/$'), 'password_reset_done', name='password_reset_done'),
-    # Support old style base36 password reset links; will be removed in Django 1.7
-    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'password_reset_confirm_uidb36', name='password_reset_confirm'),
-    url(_(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'), 'password_reset_confirm', name='password_reset_confirm'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'password_reset_confirm',
-        name='password_reset_confirm'),
     url(_(r'^password/reset/complete/$'), 'password_reset_complete', name='password_reset_complete'),
     url(_(r'^sso/$'), RedirectView.as_view(url=reverse_lazy('admin:admin_sso_openiduser_start')), name='sso_login'),
 )
+
+print urlpatterns
+
+# Support uidb36/64 compatibility across versions of Django.
+if django.VERSION[:2] >= (1, 6):
+    # 1.6+ have password_reset_confirm for uidb64.
+    # 1.6 also has the _uidb36 version, which will be removed in Django 1.7.
+    urlpatterns += patterns('django.contrib.auth.views',
+        url(_(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
+            'password_reset_confirm_uidb36', name='password_reset_confirm'),
+        url(_(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
+            'password_reset_confirm', name='password_reset_confirm'),
+    )
+
+else:
+    # <1.6 versions only have uidb36.
+    urlpatterns += patterns('django.contrib.auth.views',
+        url(_(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
+        'password_reset_confirm', name='password_reset_confirm'),
+    )
+
+
+print urlpatterns
