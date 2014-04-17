f27f3f3de35b50a5c5f89a36281d52d532f05839
adam-incuna <sortoflikechess@gmail.com>
2014-04-17 16:44:49 +0100

    Add absolute imports.
    Python 3 is sad when attempting to use this module.  This will make it happier.

--- incuna_auth/middleware/__init__.py
+++ incuna_auth/middleware/__init__.py
@@ -1,5 +1,6 @@
-from basic_auth import BasicAuthenticationMiddleware
-from login_required import LoginRequiredMiddleware
+from __future__ import absolute_import
+from .basic_auth import BasicAuthenticationMiddleware
+from .login_required import LoginRequiredMiddleware
 
 
 __all__ = ['BasicAuthenticationMiddleware', 'LoginRequiredMiddleware']
