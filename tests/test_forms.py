d5fe37f461f9b32cd7f32146e42f9772bb496d13
Adam Thomas <sortoflikechess@gmail.com>
2014-04-23 15:42:38 +0100

    Add test for CrispyPasswordResetForm
    * The test asserts the number of children of each non-leaf
      node in the tree of layout objects, then the type and
      contents of each leaf.

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,5 +1,26 @@
 from unittest import TestCase
 
+from crispy_forms.layout import Field, Submit
+
+from incuna_auth import forms
+
 
 class TestCrispyPasswordResetForm(TestCase):
-    pass
+
+    def test_form(self):
+        form = forms.CrispyPasswordResetForm()
+        layout = form.helper.layout
+
+        # The shape of the tree of elements overall
+        self.assertEqual(len(layout), 1)
+        div = layout[0]
+        self.assertEqual(len(div), 2)
+
+        # The Field element
+        self.assertTrue(isinstance(div[0], Field))
+        self.assertEqual(div[0].fields, ['email'])
+
+        # The Submit element
+        self.assertTrue(isinstance(div[1], Submit))
+        self.assertEqual(div[1].name, 'submit')
+        self.assertEqual(div[1].value, 'Reset')
