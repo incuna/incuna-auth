2ccbfe62c4022acafd9cee70d60de0b72233284b
Adam Thomas <sortoflikechess@gmail.com>
2014-04-25 14:45:54 +0100

    Add the rest of the tests for backends.py
    * The user can authenticate by username or by email,
      so test both.
    * They might have a correct username but a wrong
      password, so check that branch as well.  (Testing
      correct email + wrong password seems redundant,
      so I've left that out.)
    * To do all this, we need to be able to make users,
      so let's depend on factory_boy and add a
      UserFactory.

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from incuna_auth.backends import CustomUserModelBackend
+from factories import UserFactory
 
 
 class TestCustomUserModelBackend(TestCase):
@@ -8,4 +9,31 @@ class TestCustomUserModelBackend(TestCase):
 
     def test_nonexistent_user(self):
         result = self.backend.authenticate('fake-user', 'fake-password')
+        self.assertEqual(result, None)
+
+    def test_user_by_username(self):
+        username = 'user'
+        password = 'pass'
+        user = UserFactory.create(username=username)
+        user.set_password(password)
+        user.save()
+        result = self.backend.authenticate(username, password)
+        self.assertEqual(result, user)
+
+    def test_user_by_email(self):
+        email = 'user2@name.com'
+        password = 'pass'
+        user = UserFactory.create(email=email)
+        user.set_password(password)
+        user.save()
+        result = self.backend.authenticate(email, password)
+        self.assertEqual(result, user)
+
+    def test_user_wrong_password(self):
+        username = 'user3'
+        password = 'pass'
+        user = UserFactory.create(username=username)
+        user.set_password(password)
+        user.save()
+        result = self.backend.authenticate(username, 'wrong_password')
         self.assertEqual(result, None)
