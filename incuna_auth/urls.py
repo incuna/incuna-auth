7b8be6a9e62064362eb3b803a60623fb1bb18043
Max Peterson <max@incuna.com>
2020-05-15 18:11:51 +0100

    Restore (path) to url for backwards compatibility.

--- incuna_auth/urls.py
+++ incuna_auth/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib.auth import views
-from django.urls import get_callable, path, reverse_lazy
+from django.urls import get_callable, reverse_lazy
 from django.utils.translation import ugettext_lazy
 from django.views.generic import RedirectView
 
@@ -30,45 +30,49 @@ reset_form = get_callable(password_reset_form)
 
 
 urlpatterns = [
-    path(
-        _('login/'),
+    url(
+        _(r'^login/$'),
         views.LoginView.as_view(authentication_form=auth_form),
         name='login',
     ),
-    path(
-        _('logout/'),
+    url(
+        _(r'^logout/$'),
         views.LogoutView.as_view(template_name='registration/logout.html'),
         name='logout',
     ),
 
-    path(
-        _('password/change/'),
+    url(
+        _(r'^password/change/$'),
         views.PasswordChangeView.as_view(),
         name='password_change',
     ),
-    path(
-        _('password/change/done/'),
+    url(
+        _(r'^password/change/done/$'),
         views.PasswordChangeDoneView.as_view(),
         name='password_change_done',
     ),
 
-    path(
-        _('password/reset/'),
+    url(
+        _(r'^password/reset/$'),
         views.PasswordResetView.as_view(form_class=reset_form),
         name='password_reset',
     ),
-    path(
-        _('password/reset/done/'),
+    url(
+        _(r'^password/reset/done/$'),
         views.PasswordResetDoneView.as_view(),
         name='password_reset_done',
     ),
-    path(
-        _('password/reset/confirm/<uidb64>/<token>/'),
+    url(
+        _(
+            r'^password/reset/confirm/' +
+            r'(?P<uidb64>[0-9A-Za-z_\-]+)/' +
+            r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
+        ),
         views.PasswordResetConfirmView.as_view(),
         name='password_reset_confirm',
     ),
-    path(
-        _('password/reset/complete/'),
+    url(
+        _(r'^password/reset/complete/$'),
         views.PasswordResetCompleteView.as_view(),
         name='password_reset_complete',
     ),
